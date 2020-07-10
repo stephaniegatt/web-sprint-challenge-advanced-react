10a7378de6066779bf8b809234760fe17258100d
Stephanie Gatt <stephanielynngatt@gmail.com>
2020-07-10 11:35:58 -0400

    fetched data

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  state = {
+    plants: [],
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3333/plants").then(res => {
+      this.setState({
+        ...this.state,
+        plants: res.data.plantsData,
+      })
+      console.log("data", res.data.plantsData)
+    })
+  }
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
