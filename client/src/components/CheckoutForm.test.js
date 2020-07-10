b3fb221446b5e88b381496a3b7f46267fbab21f1
Stephanie Gatt <stephanielynngatt@gmail.com>
2020-07-10 13:10:13 -0400

    useForm finished and tests were successful

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/checkout form/i)
+    // assert
+    // make an assertion (using Jest global function)
+    expect(header).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const successText = /🎉/;
+    const sucessMessage = screen.queryByText(successText);
+    expect(sucessMessage).not.toBeInTheDocument();
+
+    const checkoutElements = screen.getAllByText(/checkout/i);
+    expect(checkoutElements).toHaveLength(2);
+    fireEvent.click(checkoutElements[1]);
+    
+
+    screen.getByText(successText);
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "../hooks/useLocalStorage";
+// import { useEffect } from "react";
+
+export const useForm = (key, initialValue) => {
+    const [values, setValues] = useLocalStorage(key, initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+    return [values, handleChanges]
+}
